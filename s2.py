7e6b2c8d9228385f78bcd92a838ae818838a6b64
Peppa Pig <xugaohero@163.com>
2018-09-12 10:39:28 +0800

    test pycharm

--- s2.py
+++ s2.py
@@ -22,15 +22,25 @@
 #            │ ─┤ ─┤       │ ─┤ ─┤
 #            └──┴──┘       └──┴──┘
 #                神兽保佑
-#                BUG是不可能有BUG的!
-n = input("please input number:")
-n = int(n)
-count = 0
-while(n != 1):
-    if(n % 2 == 0):
-        n = n / 2
-    else:
-        n = (3 * n + 1) / 2
-    count += 1
-    continue
-print(count)
+#
+i=0
+def fun(n):
+    global i
+
+    if(n%2==0):
+        n=n/2
+        fun(n)
+
+        return i+1
+
+    elif (n%2!=0 and n!=1):
+        n=(3*n+1)/2
+        fun(n)
+        return i+1
+    elif (n==1):
+        return i+1
+
+s=fun(10)
+print(s)
+
+
