7a7ea4888e6d30a21e590dd2c28f67a365f784f7
Dylan <xugaohero@163.com>
2018-10-11 11:09:13 +0800

    update

--- Undefined/demo01.py
+++ Undefined/demo01.py
@@ -27,7 +27,10 @@ import os
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
+import pandas as pd
+from numpy import *
 import matplotlib.pyplot as plt
-import numpy as np
-plt.plot(np.arange(10))
+ts = pd.Series(random.randn(1000), index=pd.date_range('1/1/2000', periods=1000))
+ts = ts.cumsum()
+ts.plot()
 plt.show()
