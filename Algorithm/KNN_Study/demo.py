146eee40d65d330966b394bfe19ac0e88dce320e
Dylan <xugaohero@163.com>
2018-10-11 23:04:41 +0800

    update

---
+++ Algorithm/KNN_Study/demo.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+#!-*-coding:utf-8 -*-
+#!@Author:xugao
+#         ┌─┐       ┌─┐
+#      ┌──┘ ┴───────┘ ┴──┐
+#      │                 │
+#      │                 │
+#      │    ＞  　　＜    │
+#      │                 │
+#      │  ....　⌒　....　│
+#      │                 │
+#      └───┐         ┌───┘
+#          │         │
+#          │         │
+#          │         │
+#          │         └──────────────┐
+#          │                        │
+#          │                        ├─┐
+#          │                        ┌─┘
+#          │                        │
+#          └─┐  ┐  ┌───────┬──┐  ┌──┘
+#            │ ─┤ ─┤       │ ─┤ ─┤
+#            └──┴──┘       └──┴──┘
+#                神兽保佑
+#                BUG是不可能有BUG的!
+import os
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+import kNN
+from numpy import *
+import operator
+
+group,labels = kNN.CreateDataSet()
+# print(group,labels)
+def classify0(inX, DataSet, labels, k):
+    dataSetSize = DataSet.shape[0]
+    diffMat = tile(inX, (dataSetSize, 1)) - DataSet
+    sqDiffMat = diffMat ** 2
+    sqDistances = sqDiffMat.sum(axis = 1)
+    distances = sqDistances ** 0.5
+    sortedDistIndicies = distances.argsort()
+    classCount = {}
+    for i in range(k):
+        voteIlabel = labels[sortedDistIndicies[i]]
+        classCount[voteIlabel] = classCount.get(voteIlabel, 0) + 1
+    sortedClassCount = sorted(classCount.items(), key = operator.itemgetter(1), reverse=True)
+    return sortedClassCount[0][0]
+
+print(classify0([0,0], group, labels, 3))
---
+++ Algorithm/KNN_Study/kNN.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+#!-*-coding:utf-8 -*-
+#!@Author:xugao
+#         ┌─┐       ┌─┐
+#      ┌──┘ ┴───────┘ ┴──┐
+#      │                 │
+#      │                 │
+#      │    ＞  　　＜    │
+#      │                 │
+#      │  ....　⌒　....　│
+#      │                 │
+#      └───┐         ┌───┘
+#          │         │
+#          │         │
+#          │         │
+#          │         └──────────────┐
+#          │                        │
+#          │                        ├─┐
+#          │                        ┌─┘
+#          │                        │
+#          └─┐  ┐  ┌───────┬──┐  ┌──┘
+#            │ ─┤ ─┤       │ ─┤ ─┤
+#            └──┴──┘       └──┴──┘
+#                神兽保佑
+#                BUG是不可能有BUG的!
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+from numpy import *
+
+def CreateDataSet():
+    group = array([[1.0, 1.1],[1.1, 1.0],[0, 0],[0, 0.1]])
+    lables = ['A','A','B','B']
+    return group,lables
