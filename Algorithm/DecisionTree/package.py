bb2ae49b48c3de0c9f0f447d53cb6d1a69dd62d8
Echo002 <xugaohero@163.com>
2018-10-24 23:01:48 +0800

    update

---
+++ Algorithm/DecisionTree/package.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+#!-*-coding:utf-8 -*-
+#!@Author:xugao
+#         ┌─┐       ┌─┐
+#      ┌──┘ ┴───────┘ ┴──┐
+#      │                 │
+#      │                 │
+#      │    ＞  　　＜    │
+#      │                 │
+#      │  ....　⌒　....　│
+#      │                 │
+#      └───┐         ┌───┘
+#          │         │
+#          │         │
+#          │         │
+#          │         └──────────────┐
+#          │                        │
+#          │                        ├─┐
+#          │                        ┌─┘
+#          │                        │
+#          └─┐  ┐  ┌───────┬──┐  ┌──┘
+#            │ ─┤ ─┤       │ ─┤ ─┤
+#            └──┴──┘       └──┴──┘
+#                神兽保佑
+#                BUG是不可能有BUG的!
+import os
+from math import log
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+# 程序清单3-1 计算给定数据集的香农熵
+def calcShannonEnt(dataSet):
+    numEntires = len(dataSet)
+    labelCounts = {}
+    for featVec in dataSet:
+        currentLabel = featVec[-1]
+        if currentLabel not in labelCounts.keys():
+            labelCounts[currentLabel] = 0
+        labelCounts[currentLabel] += 1
+    shannonEnt = 0.0
+    for key in labelCounts:
+        prob = float(labelCounts[key])/numEntires
+        shannonEnt -= prob * log(prob, 2)
+    return shannonEnt
