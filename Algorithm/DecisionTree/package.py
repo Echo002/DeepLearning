39f38eb7cb0bc2b71c5009618b6b0d37938fd631
Echo002 <xugaohero@163.com>
2018-10-25 21:21:00 +0800

    update

--- Algorithm/DecisionTree/package.py
+++ Algorithm/DecisionTree/package.py
@@ -27,6 +27,13 @@ import os
 from math import log
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
+dataset = [[1, 1, 'yes'],
+            [1, 1, 'yes'],
+            [1, 0, 'no'],
+            [0, 1, 'no'],
+            [0, 1, 'no']]
+labels = ['no surfacing', 'flippers']
+
 # 程序清单3-1 计算给定数据集的香农熵
 def calcShannonEnt(dataSet):
     numEntires = len(dataSet)
@@ -41,3 +48,14 @@ def calcShannonEnt(dataSet):
         prob = float(labelCounts[key])/numEntires
         shannonEnt -= prob * log(prob, 2)
     return shannonEnt
+print(calcShannonEnt(dataset))
+
+# 程序清单3-2 按照给定特征划分数据集
+def splitDataSet(dataSet, axis, value):
+    retDataset = []
+    for featVec in dataSet:
+        if featVec[axis] == value:
+            reducedFeatVec = featVec[:axis]
+            reducedFeatVec.extend(featVec[axis+1:])
+            retDataset.append(reducedFeatVec)
+    return retDataset
