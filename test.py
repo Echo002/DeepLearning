31bf9b258cfbf8d32b9d48b8e48efc69618dbd9d
Echo002 <xugaohero@163.com>
2018-11-01 16:25:07 +0800

    update

--- test.py
+++ test.py
@@ -34,26 +34,26 @@ leafNode = dict(boxstyle = "round4", fc = "0.8")
 arrow_args = dict(arrowstyle = "<-")
 
 def plotNode(nodeTxt, centerPt, parentPt, nodeType):
-    createPlot.ax1.annotate(nodeTxt, xy=parentPt,  xycoords='axes fraction',
+    createPolt.ax1.annotate(nodeTxt, xy=parentPt,  xycoords='axes fraction',
                             xytext=centerPt, textcoords='axes fraction',
                             va="center", ha="center", bbox=nodeType, arrowprops=arrow_args )
 
-def createPlot(inTree):
+def createPolt(inTree):
     fig = plt.figure(1, facecolor='white')
     fig.clf()
     axprops = dict(xticks=[], yticks=[])
-    createPlot.ax1 = plt.subplot(111, frameon=False, **axprops)
+    createPolt.ax1 = plt.subplot(111, frameon=False, **axprops)
     plotTree.totalW = float(getNumLeafs(inTree))
     plotTree.totalD = float(getTreeDepth(inTree))
-    plotTree.xOff = -0.5 / plotTree.totalW
-    plotTree.yOff = 1.0
+    plotTree.xOff = -0.5 / plotTree.totalW;
+    plotTree.yOff = 1.0;
     plotTree(inTree, (0.5, 1.0), '')
     plt.show()
 
 def getNumLeafs(myTree):
     numLeafs = 0
-    firstSide = list(myTree.keys())
-    firstStr = firstSide[0]
+    firstSides = list(myTree.keys())
+    firstStr = firstSides[0]
     secondDict = myTree[firstStr]
     for key in secondDict.keys():
         if type(secondDict[key]).__name__=='dict':
@@ -63,8 +63,8 @@ def getNumLeafs(myTree):
 
 def getTreeDepth(myTree):
     maxDepth = 0
-    firstSide = list(myTree.keys())
-    firstStr = firstSide[0]
+    firstSides = list(myTree.keys())
+    firstStr = firstSides[0]
     secondDict = myTree[firstStr]
     for key in secondDict.keys():
         if type(secondDict[key]).__name__=='dict':
@@ -82,7 +82,7 @@ def retrieveTree(i):
 def plotMidText(cntrPt, parentPt, txtString):
     xMid = (parentPt[0]-cntrPt[0])/2.0 + cntrPt[0]
     yMid = (parentPt[1]-cntrPt[1])/2.0 + cntrPt[1]
-    createPlot.ax1.text(xMid, yMid, txtString, va="center", ha="center", rotation=30)
+    createPolt.ax1.text(xMid, yMid, txtString, va="center", ha="center", rotation=30)
 
 def plotTree(myTree, parentPt, nodeTxt):
     numLeafs = getNumLeafs(myTree)
@@ -104,4 +104,4 @@ def plotTree(myTree, parentPt, nodeTxt):
     plotTree.yOff = plotTree.yOff + 1.0/plotTree.totalD
 
 myTree = retrieveTree(0)
-createPlot(myTree)
+createPolt(myTree)
