f8007053f787b780016064eb850b323bd3d69c77
Echo002 <xugaohero@163.com>
2018-12-13 21:56:06 +0800

    update

---
+++ Package/05 Pytorch/06 保存和提取.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+#!-*-coding:utf-8 -*-
+#!@Author:xugao
+#         ┌─┐       ┌─┐
+#      ┌──┘ ┴───────┘ ┴──┐
+#      │                 │
+#      │                 │
+#      │    ＞  　　＜    │
+#      │                 │
+#      │  ....　⌒　....　│
+#      │                 │
+#      └───┐         ┌───┘
+#          │         │
+#          │         │
+#          │         │
+#          │         └──────────────┐
+#          │                        │
+#          │                        ├─┐
+#          │                        ┌─┘
+#          │                        │
+#          └─┐  ┐  ┌───────┬──┐  ┌──┘
+#            │ ─┤ ─┤       │ ─┤ ─┤
+#            └──┴──┘       └──┴──┘
+#                神兽保佑
+#                BUG是不可能有BUG的!
+import os
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+import torch
+from torch.autograd import Variable
+import matplotlib.pyplot as plt
+
+# torch.manual_seed(1)
+
+x = torch.unsqueeze(torch.linspace(-1, 1, 100), dim=1)
+y = x.pow(2) + 0.2*torch.rand(x.size())
+# x, y = Variable(x, requires_grad=False), Variable(y, requires_grad=False)
+
+def save():
+    net1 = torch.nn.Sequential(
+        torch.nn.Linear(1, 10),
+        torch.nn.ReLU(),
+        torch.nn.Linear(10, 1)
+    )
+    optimizer = torch.optim.SGD(net1.parameters(), lr=0.5)
+    loss_func = torch.nn.MSELoss()
+
+    for t in range(100):
+        prediction = net1(x)
+        loss = loss_func(prediction ,y)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+    plt.figure(1, figsize=(10, 3))
+    plt.subplot(131)
+    plt.title('Net1')
+    plt.scatter(x.data.numpy(), y.data.numpy())
+    plt.plot(x.data.numpy(), prediction.data.numpy(), 'r-', lw=5)
+
+    torch.save(net1, 'net.pkl')  # 保存整个网络
+    torch.save(net1.state_dict(), 'net_params.pkl')  # 保存所有的参数
+
+def restore_net():
+    net2 = torch.load('net.pkl')
+    prediction = net2(x)
+    plt.subplot(132)
+    plt.title('Net2')
+    plt.scatter(x.data.numpy(), y.data.numpy())
+    plt.plot(x.data.numpy(), prediction.data.numpy(), 'r-', lw=5)
+    plt.show()
+
+def restore_param():
+    # 因为只保留了参数，所以需要完成搭建和模型一样的神经网络框架
+    net3 = torch.nn.Sequential(
+        torch.nn.Linear(1, 10),
+        torch.nn.ReLU(),
+        torch.nn.Linear(10, 1)
+    )
+    prediction = net3(x)
+    net3.load_state_dict(torch.load('net_params.pkl'))
+    plt.subplot(133)
+    plt.title('Net3')
+    plt.scatter(x.data.numpy(), y.data.numpy())
+    plt.plot(x.data.numpy(), prediction.data.numpy(), 'r-', lw=5)
+    plt.show()
+
+save()
+restore_net()
+restore_param()
---
+++ Package/05 Pytorch/07 批训练.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+#!-*-coding:utf-8 -*-
+#!@Author:xugao
+#         ┌─┐       ┌─┐
+#      ┌──┘ ┴───────┘ ┴──┐
+#      │                 │
+#      │                 │
+#      │    ＞  　　＜    │
+#      │                 │
+#      │  ....　⌒　....　│
+#      │                 │
+#      └───┐         ┌───┘
+#          │         │
+#          │         │
+#          │         │
+#          │         └──────────────┐
+#          │                        │
+#          │                        ├─┐
+#          │                        ┌─┘
+#          │                        │
+#          └─┐  ┐  ┌───────┬──┐  ┌──┘
+#            │ ─┤ ─┤       │ ─┤ ─┤
+#            └──┴──┘       └──┴──┘
+#                神兽保佑
+#                BUG是不可能有BUG的!
+import os
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+import torch
+# 批训练模块
+import torch.utils.data as Data
+
+BATCH_SIZE = 5
+
+x = torch.linspace(1, 10, 10)
+y = torch.linspace(10, 1, 10)
+
+torch_dataset = Data.TensorDataset(x, y)
+loader = Data.DataLoader(
+    dataset=torch_dataset,
+    batch_size=BATCH_SIZE,
+    shuffle=False,
+    # 是否随机顺序
+    num_workers = 2
+    # 采取多线程
+)
+
+def show_batch():
+    for epoch in range(3):   # train entire dataset 3 times
+        for step, (batch_x, batch_y) in enumerate(loader):  # for each training step
+            # train your data...
+            print('Epoch: ', epoch, '| Step: ', step, '| batch x: ',
+                  batch_x.numpy(), '| batch y: ', batch_y.numpy())
+
+
+if __name__ == '__main__':
+    show_batch()
