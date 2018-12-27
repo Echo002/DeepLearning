3a7765e6d091b9d83d0f6afe3d1eb746549cd899
Echo002 <xugaohero@163.com>
2018-12-27 23:02:20 +0800

    update

--- Undefined/demo01.py
+++ Undefined/demo01.py
@@ -1,9 +1,13 @@
 import numpy as np
 
 x_train = np.array([[1,2,3],[4,5,6]])
-m = len(x_train)
-result = np.zeros(m,10)
-for i,j in enumerate(x_train):
-    print(result)
-    result[i,j] = 1
-    print(result)
+# m = len(x_train)
+# result = np.zeros((m,10))
+# print(result)
+# for i,j in enumerate(x_train):
+#     print(i,j)
+#     result[i,j] = 1
+# print(result)
+
+x_train[0, [1,2]] = 1
+print(x_train)
