6eaed6e6f55e755db746d02c671da0812b9831eb
Echo <xugaohero@163.com>
2019-07-09 22:39:41 +0800

    update

--- Undefined/test.py
+++ Undefined/test.py
@@ -1,10 +1,12 @@
 import tensorflow as tf
-import keras
-with tf.device('/gpu:0'):
-    a = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[2, 3], name='a')
-    b = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[3, 2], name='b')
-    c = tf.matmul(a, b)
+a = tf.constant([1.0, 2.0, 3.0], shape=[3], name='a')
+b = tf.constant([1.0, 2.0, 3.0], shape=[3], name='b')
+with tf.device('/gpu:1'):
+    c = a + b
 
-with tf.Session() as sess:
-    print (sess.run(c))
-#test
+# 注意：allow_soft_placement=True表明：计算设备可自行选择，如果没有这个参数，会报错。
+# 因为不是所有的操作都可以被放在GPU上，如果强行将无法放在GPU上的操作指定到GPU上，将会报错。
+sess = tf.Session(config=tf.ConfigProto(allow_soft_placement=True, log_device_placement=True))
+# sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
+sess.run(tf.global_variables_initializer())
+print(sess.run(c))
